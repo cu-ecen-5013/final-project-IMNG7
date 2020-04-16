fedbdf1edaff79227b71fa4e8303f21f6a821694
IMNG7 <nigu8044@colorado.edu>
2020-04-16 00:27:32 -0600

    Leveraged Code commit

--- project_dependencies.sh
+++ project_dependencies.sh
@@ -1,9 +1,18 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install python-opencv
-sudo apt-get install libopencv-dev
-sudo apt-get install build-essential cmake
-sudo apt-get install zlib1g-dev libwebp-dev libpng-dev libjasper-dev libtiff5-d$
-sudo apt-get install qt5-default libvtk6-dev
+#sudo apt-get install python-opencv
+sudo apt-get install cmake
+sudo apt-get install gcc g++
+sudo apt-get install python-dev python-numpy   
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev 
+sudo apt-get install libgtk-3-dev
+sudo apt-get install libpng-dev
+sudo apt-get install libjpeg-dev
+sudo apt-get install libopenexr-dev
+sudo apt-get install libtiff-dev
+sudo apt-get install libwebp-dev
 sudo apt-get install python-pip
 pip install --upgrade imutils
+pip3 install matplotlib
+pip install argparse
