e646335b0de38bc9830305e10d1d12d3d3351f58
IMNG7 <nigu8044@colorado.edu>
2020-04-25 21:31:45 -0600

    Adding everything outside

--- people_detection_test.sh
+++ people_detection_test.sh
@@ -1,3 +1,3 @@
 python people_detection.py \
 	--prototxt MobileNetSSD_deploy.prototxt \
-	--model MobileNetSSD_deploy.caffemodel --output output/output_01.avi
+	--model MobileNetSSD_deploy.caffemodel 
